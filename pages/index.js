e2418eadfb96d3a18931eb59ac463e733b7eb18c
Martin Šíl <sil@spstrutnov.cz>
2022-11-30 20:29:25 +0100

    Připojení ke Stripe

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { checkout } from "../checkout"
 
 export default function Home() {
   return (
@@ -17,7 +18,7 @@ export default function Home() {
         </h1>
 
         <div className={styles.grid}>
-          <a href="" className={styles.card}>
+          <div className={styles.card}>
           <Image 
             src="/img/tenisky/teniska1.png" 
             alt="Teniska Jordan 1 Mid GS Fire Red"
@@ -25,9 +26,19 @@ export default function Home() {
             height={673}/>
             <h2>Jordan 1 Mid GS Fire Red</h2>
             <p>5.499 Kč</p>
-          </a>
+            <button onClick={(() => {
+                checkout({
+                  lineItems: [
+                    {
+                      price: "price_1M9umIBMpREfxNmDKPZGpeZq",
+                      quantity: 1
+                    }
+                  ]
+                })
+              })}>Koupit!</button>
+          </div>
 
-          <a href="" className={styles.card}>
+          <div className={styles.card}>
             <Image 
               src="/img/tenisky/teniska2.png" 
               alt="Teniska Jordan 1 Mid Stealth"
@@ -35,7 +46,17 @@ export default function Home() {
               height={673}/>
             <h2>Jordan 1 Mid Stealth</h2>
             <p>4.999 Kč</p>
-          </a>
+              <button onClick={(() => {
+                checkout({
+                  lineItems: [
+                    {
+                      price: "price_1M9unLBMpREfxNmDg2f9UK0E",
+                      quantity: 1
+                    }
+                  ]
+                })
+              })}>Koupit!</button>
+          </div>
         </div>
       </main>
 
